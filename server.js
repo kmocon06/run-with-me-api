2832527303235b144f1cefed140203f92e793220
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-02-24 16:23:35 -0600

    able to store user sessions with express-sessions

--- server.js
+++ server.js
@@ -3,15 +3,36 @@ require('dotenv').config()
 const express = require('express')
 const server = express()
 const PORT = process.env.PORT
+//Parse incoming request bodies in a middleware before your handlers, 
+//available under the req.body property with body-parser
+const bodyParser = require('body-parser')
+//be able to store sessions for each user
+const session = require('express-sessions')
 
 
-//routes
+//MIDDLEWARE
+
+//body-parser
+server.use(bodyParser.urlencoded({extended: false}))
+//express-sessions
+//this makes req.sessions accessible 
+server.use(session({
+	//scramble session info so someone is unable 
+	//to breach system and access session data
+	secret: process.env.SESSION_SECRET,
+	resave: false, 
+  	saveUninitialized: false 
+}))
+
+
+
+//ROUTES
 server.get('/', (req, res) => {
   res.send('RUN WITH ME PROJECT')
 })
 
 
-//listener
+//LISTENERS
 server.listen(PORT, () => {
   const currentDate = new Date()
   console.log(`${currentDate.toLocaleString()}: Server listening on port ${PORT}`)
