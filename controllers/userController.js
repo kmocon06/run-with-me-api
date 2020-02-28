7116403468ac4ef0587da777175e544d8e3a0f3b
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-02-28 11:30:56 -0600

    can see specific workout with ID for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,17 +5,31 @@ const User = require('../models/user.js')
 
 // GET /users/:userId/workouts -- lists all the workouts for one user
 router.get('/:userId/workouts', async (req, res, next) => {
-  const workoutsForThisUser = await Workout.find({user: req.params.userId})
-  // you could also get this information with populate('owner') on the
-  // prev query
+  const workoutsForThisUser = await Workout.find({
+  	user: req.params.userId})
   	const user = await User.findById(req.params.userId)
 
 
-  	console.log(workoutsForThisUser)
-  	console.log(user)
+  	//console.log(workoutsForThisUser)
+  	//console.log(user)
   	res.status(200).send({
 		data: workoutsForThisUser,
 		message: 'We can see all of the workouts for the user!'
+	})
+})
+
+// GET /users/:userId/workouts -- lists one workout for one user
+router.get('/:userId/workouts/:id', async (req, res, next) => {
+  // const workoutsForThisUser = await Workout.find({
+  // 	user: req.params.userId}).populate('workouts')
+  // 	const user = await User.findById(req.params.userId)
+
+  	const oneUserWorkout = await Workout.findById(req.params.id)
+
+  	console.log(oneUserWorkout)
+  	res.status(200).send({
+		data: oneUserWorkout,
+		message: `We can see the specific workout with id of ${req.params.id} for the user!`
 	})
 })
 
