15fe7e2d4c532e8a56772a9d9404693ae4cfae32
Krystina O'Connor <krystina.m.oconnor@gmail.com>
2020-03-04 17:34:29 -0600

    removed )

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/run-with-me')
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/run-with-me'
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true, 
